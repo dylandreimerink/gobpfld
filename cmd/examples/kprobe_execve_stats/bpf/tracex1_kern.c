80dcc8de2eb9b31f5b0ba73ea9de02033b8b90af
Dylan Reimerink <dylan@serverius.net>
2021-10-27 22:09:36 +0200

    Rewrite kprobe example so we don't have to include huge headerfiles

--- cmd/examples/kprobe_execve_stats/bpf/tracex1_kern.c
+++ cmd/examples/kprobe_execve_stats/bpf/tracex1_kern.c
@@ -1,12 +1,25 @@
 #include "vmlinux.h"
-#include "bpf_helpers.h"
 
-struct bpf_map_def SEC("maps") execve_stats = {
-	.type        = BPF_MAP_TYPE_ARRAY,
-	.key_size    = sizeof(__u32),
-	.value_size  = sizeof(__u64),
-	.max_entries = 1,
+/*
+ * bpf_map_lookup_elem
+ *
+ * 	Perform a lookup in *map* for an entry associated to *key*.
+ *
+ * Returns
+ * 	Map value associated to *key*, or **NULL** if no entry was
+ * 	found.
+ */
+static void *(*bpf_map_lookup_elem)(void *map, const void *key) = (void *) 1;
+
+struct bpf_map_def {
+	unsigned int type;
+	unsigned int key_size;
+	unsigned int value_size;
+	unsigned int max_entries;
+	unsigned int map_flags;
 };
+
+#define SEC(name) __attribute__((section(name), used)) 
 
 /* LLVM maps __sync_fetch_and_add() as a built-in function to the BPF atomic add
  * instruction (that is BPF_STX | BPF_XADD | BPF_W for word sizes)
@@ -15,6 +28,12 @@ struct bpf_map_def SEC("maps") execve_stats = {
 #define lock_xadd(ptr, val)	((void) __sync_fetch_and_add(ptr, val))
 #endif
 
+struct bpf_map_def SEC("maps") execve_stats = {
+	.type        = BPF_MAP_TYPE_ARRAY,
+	.key_size    = sizeof(__u32),
+	.value_size  = sizeof(__u64),
+	.max_entries = 1,
+};
 
 /* kprobe is NOT a stable ABI
  * kernel functions can be removed, renamed or completely change semantics.
